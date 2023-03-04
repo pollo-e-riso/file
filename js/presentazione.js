c2c96a5ced92cd65de6087e50a0360222272f074
ioriaaa <dravaix@gmail.com>
2023-03-04 09:11:29 +0100

    terrrone

--- js/presentazione.js
+++ js/presentazione.js
@@ -1,6 +1,15 @@
-$(document).ready(function (){
-    $("#start").click(function (){
+$(document).ready(function() {
+    $("#start").click(function() {
+      $("#start").removeClass("btn btn-warning active");
+      $("#start").addClass("btn btn-warning disabled");
+        $("#start").html("Caricamento in corso...");
+
+        $("#pacman").animate({"left": "+=210%"}, 4000);
+        $(".rettangolo").animate({"width": "+=210%"}, 4000);
+      setTimeout(function() {
         window.location.href = 'pacman.html';
+      }, 4000);
     });
-});
+  });
+
 
