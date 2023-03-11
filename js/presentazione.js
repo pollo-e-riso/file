00cd89a465ee6444951c96ce9770e2bb82218af7
ioriaaa <dravaix@gmail.com>
2023-03-11 10:07:59 +0100

    tanti

--- js/presentazione.js
+++ js/presentazione.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     $("#start").click(function() {
-      $("#start").removeClass("btn btn-warning active");
-      $("#start").addClass("btn btn-warning disabled");
         $("#start").html("Caricamento in corso...");
+        $("#start").css("border-radius", "10px");
 
-        $("#pacman").animate({"left": "+=210%"}, 4000);
-        $(".rettangolo").animate({"width": "+=210%"}, 4000);
+        $(".pacman").animate({"left": "+=150%"}, 4000);
+        $(".rettangolo").animate({"width": "+=150%"}, 4000);
+
+        $(".pacmanD").animate({"left": "-=150%"}, 4000);
+        $(".rettangoloD").animate({"width": "+=150%"}, 4000);
+
       setTimeout(function() {
         window.location.href = 'pacman.html';
       }, 4000);
