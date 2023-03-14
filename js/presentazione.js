4a0537ff4548ab0b2a4543c76f7624918c96f4b4
ioriaaa <dravaix@gmail.com>
2023-03-14 12:23:18 +0100

    jk

--- js/presentazione.js
+++ js/presentazione.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $("#start").click(function() {
-        $("#start").html("Caricamento in corso...");
+        $("#start").css("left", "35%");
+        $("#start").html("<i class = 'fa fa-refresh fa-spin'></i> Loading...");
         $("#start").css("border-radius", "10px");
 
         $(".pacman").animate({"left": "+=150%"}, 4000);
