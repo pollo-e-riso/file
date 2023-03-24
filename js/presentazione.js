762cd6f7c4c6076ab5f9023d0bd27dc9dc307525
ioriaaa <dravaix@gmail.com>
2023-03-24 20:12:49 +0100

    audio

--- js/presentazione.js
+++ js/presentazione.js
@@ -1,20 +1,22 @@
+var audio = document.getElementById("myAudio");
 $(document).ready(function() {
     $("#start").click(function() {
+      audio.play();
         $("#start").html("<i class = 'fa fa-refresh fa-spin'></i> Loading...");
         $("#start").css("border-radius", "10px");
 
-        $(".pacman").animate({"left": "+=150%"}, 4000);
-        $(".rettangolo").animate({"width": "+=150%"}, 4000);
+        $(".pacman").animate({"left": "+=150%"}, 4200);
+        $(".rettangolo").animate({"width": "+=150%"}, 4200);
 
-        $(".pacmanD").animate({"left": "-=150%"}, 4000);
-        $(".rettangoloD").animate({"width": "+=150%"}, 4000);
+        $(".pacmanD").animate({"left": "-=150%"}, 4200);
+        $(".rettangoloD").animate({"width": "+=150%"}, 4200);
 
       setTimeout(function() {
         window.location.href = 'pacman.html';
-      }, 4000);
+      }, 4200);
     });
-    $("#freccia").on("click", function() {
-      $(this).animate({top: "-=50px"}, 1000);
+    $(".up").on("click", function() {
+      $(this).toggleClass("up down");
     });
   });
 
